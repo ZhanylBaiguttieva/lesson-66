489875373c2061bfd906a0ab1c575d898c55a512
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-11 03:10:41 +0600

    All functions in App.tsx remoted to Home.tsx

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,14 +1,49 @@
-import {Meat} from '../../types';
+import {Meat, MeatsList} from '../../types';
 import MeatItem from './MeatItem';
 import {NavLink} from 'react-router-dom';
+import {useCallback, useEffect, useState} from 'react';
+import axiosApi from '../../axiosApi';
 
-interface Props {
-  meats: Meat[];
-  deleteMeat:(id: string) => void;
-  meatsLoading: boolean;
-}
+const Home: React.FC= () => {
 
-const Home: React.FC<Props> = ({meats, deleteMeat}) => {
+  const [meats, setMeats] = useState<Meat[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMeats = useCallback(async () => {
+    try {
+      setLoading(true);
+      const meatsResponse = await axiosApi.get<MeatsList | null>('meats.json');
+      const meats = meatsResponse.data;
+
+      if (!meats) {
+        setMeats([]);
+        return;
+      }
+      const newMeats = Object.entries(meats).map(([meatId, meat]) => {
+        return {
+          id: meatId,
+          time: meat.time,
+          description: meat.description,
+          calories: meat.calories,
+        };
+      });
+      setMeats(newMeats);
+    } finally {
+      setLoading(false);
+    }
+  },[]);
+
+  useEffect(() => {
+    void fetchMeats();
+  }, [fetchMeats]);
+
+  const deleteMeat = async (id: string) => {
+    if(window.confirm('Do you really want to delete?')) {
+      await axiosApi.delete('meats/' + id + '.json');
+      await fetchMeats();
+    }
+  };
+
   const total = meats.reduce((sum, meat) => {
     return sum + parseFloat(meat.calories);
   }, 0);
@@ -16,11 +51,13 @@ const Home: React.FC<Props> = ({meats, deleteMeat}) => {
   return (
     <>
       <div className="text-end mb-3">
-        <NavLink className="btn btn-success" to="/new-meat" >Add new meat</NavLink>
+        {!loading &&
+          <NavLink className="btn btn-success" to="/new-meat" >Add new meat</NavLink>
+        }
       </div>
       <div>
         <h4>Meats</h4>
-        <div>Total calories:{total} kcal</div>
+        <div> <strong>Total calories:</strong> {total} kcal</div>
         {meats.map((meat) => (
           <MeatItem
             key={meat.id}
