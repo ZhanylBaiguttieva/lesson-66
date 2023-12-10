664cd1a46350574c99aec80a597ab9c6f8c11715
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-11 02:20:10 +0600

    Added Home.tsx

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,10 +1,31 @@
+import {Meat} from '../../types';
+import MeatItem from './MeatItem';
+import {NavLink} from 'react-router-dom';
 
+interface Props {
+  meats: Meat[];
+  deleteMeat:(id: string) => void;
+  meatsLoading: boolean;
+}
 
-const Home = () => {
+const Home: React.FC<Props> = ({meats, deleteMeat}) => {
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="text-end mb-3">
+        <NavLink className="btn btn-success" to="/new-meat" >Add new meat</NavLink>
+      </div>
+      <div>
+        <h4>Meats</h4>
+        {meats.map((meat) => (
+          <MeatItem
+            key={meat.id}
+            meat={meat}
+            onDelete={() => deleteMeat(meat.id)}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
