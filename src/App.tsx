45e9b9cd62435693fc7fc5d8993020475b8d9df9
Zhanyl Baiguttieva <janabaiguttieva@gmail.com>
2023-12-11 02:21:05 +0600

    Added Routes and delete function on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,72 @@
 
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import {useCallback, useEffect, useState} from 'react';
+import {Meat, MeatsList} from './types';
+import axiosApi from './axiosApi';
+import MeatForm from './containers/Form/MeatForm';
+
 
 function App() {
+  const [meats, setMeats] = useState<Meat[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMeats = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const meatsResponse = await axiosApi.get<MeatsList | null>('meats.json');
+      const meats = meatsResponse.data;
+
+      if (!meats) {
+        setMeats([]);
+        return;
+      }
+      const newMeats = Object.entries(meats).map(([meatId, meat]) => {
+        return {
+          id: meatId,
+          time: meat.time,
+          description: meat.description,
+          calories: meat.calories,
+        };
+      });
+
+      setMeats(newMeats);
+    } finally {
+      setLoading(false);
+    }
+  },[]);
+
+  useEffect(() => {
+    void fetchMeats();
+  }, [fetchMeats]);
+
+  const deleteMeat = async (id: string) => {
+    if(window.confirm('Do you really want to delete?')) {
+      await axiosApi.delete('meats/' + id + '.json');
+      await fetchMeats();
+    }
+  };
 
   return (
     <>
-        Hello!
+      <div className="w-50">
+        <Routes>
+          <Route path='/' element={(<Home
+            meatsLoading={loading}
+            meats={meats}
+            deleteMeat={deleteMeat}
+          />)}></Route>
+          <Route path='/meats' element={(<Home
+            meatsLoading={loading}
+            meats={meats}
+            deleteMeat={deleteMeat}
+          />)}></Route>
+          <Route path="/new-meat" element={(<MeatForm/>)} />
+          <Route path="/edit-meat/:meatId" element={<MeatForm/>}/>
+        </Routes>
+      </div>
     </>
   );
 }
